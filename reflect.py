4c0842a6f9ec88adf48066b7cb0745271f486e30
damacaner <caner@elephantapps.co>
2023-07-11 00:23:05 +0300

    ...and the rest of the files with an example.

--- reflect.py
+++ reflect.py
@@ -1,7 +1,9 @@
 from typing import Any, Callable
 
+
 def convert(from_value: Any, to_value: Any) -> Any:
     return type(to_value)(from_value)
+
 
 def forEach(s: Any, f: Callable[[Any], None]) -> None:
     if not isinstance(s, (list, tuple)):
